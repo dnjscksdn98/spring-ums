7f28cc2a08bd5659b02af638f6e6c2d73e57ea23
Alex <dnjscksdn98@gmail.com>
2020-04-12 20:13:15 +0900

    Create: MemberDao 생성
    MemberDao 기본 메소드 생성

---
+++ src/main/java/com/spring_ex/ums/member/model/dao/MemberDao.java
@@ -0,0 +1,18 @@
+package com.spring_ex.ums.member.model.dao;
+
+import java.util.List;
+
+import com.spring_ex.ums.member.model.dto.MemberDto;
+
+public interface MemberDao {
+	
+	public List<MemberDto> memberList();  // 회원 목록
+	
+	public void insertMember(MemberDto dto);  // 회원 추가
+	
+	public MemberDto viewMember();  // 회원 정보 상세 보기
+	
+	public void deleteMember(String userId);  // 회원 삭제
+	
+	public void updateMember(MemberDto dto);  // 회원 정보 수정
+}
