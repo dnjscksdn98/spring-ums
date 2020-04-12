bc638c8fdb00cb3a77e308abd5cd4637b326ad02
Alex <dnjscksdn98@gmail.com>
2020-04-12 22:21:07 +0900

    Make: MemberDao 구현

---
+++ src/main/java/com/spring_ex/ums/member/model/dao/MemberDaoImpl.java
@@ -0,0 +1,42 @@
+package com.spring_ex.ums.member.model.dao;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.spring_ex.ums.member.model.dto.MemberDto;
+
+
+@Repository
+public class MemberDaoImpl implements MemberDao {
+	
+	@Autowired
+	SqlSession sqlSession;
+	
+	@Override
+	public List<MemberDto> memberList(){
+		return sqlSession.selectList("memberList");
+	}
+	
+	@Override
+	public void insertMember(MemberDto dto) {
+		
+	}
+	
+	@Override
+	public MemberDto viewMember() {
+		return null;
+	}
+	
+	@Override
+	public void deleteMember(String userId) {
+		
+	}
+	
+	@Override
+	public void updateMember(MemberDto dto) {
+		
+	}
+}
