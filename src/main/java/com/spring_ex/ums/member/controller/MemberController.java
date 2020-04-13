086f862686c874cfadd1b2605751caf3d9bd63ae
Alex <dnjscksdn98@gmail.com>
2020-04-13 19:43:26 +0900

    Add: 회원 정보 삭제 처리 매핑 함수 추가
    회원정보 수정 처리에 비밀번호 체크 추가

--- src/main/java/com/spring_ex/ums/member/controller/MemberController.java
+++ src/main/java/com/spring_ex/ums/member/controller/MemberController.java
@@ -2,11 +2,12 @@ package com.spring_ex.ums.member.controller;
 
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.stereotype.Controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.spring_ex.ums.member.model.dto.MemberDto;
 import com.spring_ex.ums.member.service.MemberServiceImpl;
@@ -43,8 +44,37 @@ public class MemberController {
 	}
 	
 	@RequestMapping("member/update.do")
-	public String memberUpdate(@ModelAttribute MemberDto dto) {
-		memberService.updateMember(dto);
-		return "redirect:/member/list.do";
+	public String memberUpdate(@ModelAttribute MemberDto dto, Model model) {
+		boolean result = memberService.checkPw(dto.getUserId(), dto.getUserPw());
+		
+		if(result) {
+			memberService.updateMember(dto);
+			return "redirect:/member/list.do";
+		}
+		else {
+			MemberDto dto2 = memberService.viewMember(dto.getUserId());
+			dto.setUserRegDate(dto2.getUserRegDate());
+			dto.setUserUpdateDate(dto.getUserUpdateDate());
+			model.addAttribute("dto", dto);
+			model.addAttribute("msg", "비밀번호 불일치");
+			
+			return "member/member_view";
+		}
+	}
+	
+	@RequestMapping("member/delete.do")
+	public String memberDelete(@RequestParam String userId, @RequestParam String userPw, Model model) {
+		
+		boolean result = memberService.checkPw(userId, userPw);
+		
+		if(result) {
+			memberService.deleteMember(userId);
+			return "redirect:/member/list.do";
+		}
+		else {
+			model.addAttribute("msg", "비밀번호 불일치");
+			model.addAttribute("dto", memberService.viewMember(userId));
+			return "member/member_view";
+		}
 	}
 }
