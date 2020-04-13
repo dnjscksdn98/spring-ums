1474b9d056610601d1f6c4214bc60e6bae2baf66
Alex <dnjscksdn98@gmail.com>
2020-04-13 17:02:15 +0900

    Add: 회원 등록 페이지 이동&처리 함수 추가

--- src/main/java/com/spring_ex/ums/member/controller/MemberController.java
+++ src/main/java/com/spring_ex/ums/member/controller/MemberController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import com.spring_ex.ums.member.model.dto.MemberDto;
@@ -21,5 +22,17 @@ public class MemberController {
 		List<MemberDto> list = memberService.memberList();
 		model.addAttribute("list", list);
 		return "member/member_list";
+	}
+	
+	@RequestMapping("member/write.do")
+	public String memberWrite() {
+		return "member/member_write";
+	}
+	
+	@RequestMapping("member/insert.do")
+	public String memberInsert(@ModelAttribute MemberDto dto) {
+		memberService.insertMember(dto);
+		
+		return "redirect:/member/list.do";
 	}
 }
